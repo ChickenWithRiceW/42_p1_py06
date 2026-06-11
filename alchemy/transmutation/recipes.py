e0f35a64a07bd0a8ac7647fe9b2b3062b0681df1
Achilles Scheufele <ascheufe@2-D-8.42heilbronn.de>
2026-06-11 10:38:52 +0200

    Everything is finished

--- alchemy/transmutation/recipes.py
+++ alchemy/transmutation/recipes.py
@@ -1,11 +1,11 @@
 import alchemy.elements
-# from alchemy.elements import create_fire
-from alchemy.potions import strength_potion
+from elements import create_fire
+from ..potions import strength_potion
 
 
 def lead_to_gold() -> str:
     text = "Recipe transmuting Lead to Gold: brew " \
             f"{alchemy.elements.create_air()} and " \
             f"{strength_potion()} mixed with " \
-            f"{alchemy.elements.create_fire()}"
+            f"{create_fire()}"
     return text
